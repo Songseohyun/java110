c4ba687a66dbe5754ef77996ca4c79b5386eb179
Song <tjgus2932@gmail.com>
2018-09-04 14:39:17 +0900

    배열에담기

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -2,26 +2,40 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        String[] names = new String[100];
+        String[] emails = new String[100];
+        String[] password = new String[100];
+        int index = 0;
+        
         //1) 키보드 입력을 처리할 객체 준비
         Scanner KeyIn = new Scanner(System.in);
         //2) 사용자로부터 회원 정보 입력받기
         while(true) {
         System.out.print("이름을 입력하세요 :\t");
-        String name = KeyIn.nextLine(); //한줄입력받기 nextLine
+       names[index] = KeyIn.nextLine(); //한줄입력받기 nextLine
         
         System.out.print("이메일을 입력하세요 :\t ");
-        String email = KeyIn.nextLine();
+        emails[index] = KeyIn.nextLine();
         
         System.out.print("암호를 입력하세요 :\t"); 
-        String pwd = KeyIn.nextLine();
+        password[index] = KeyIn.nextLine();
         
-        System.out.printf("%s, %s, %s\n", name, email, pwd);
+        index++;
         
         System.out.println("계속 하시겠습니까? (Y/n) ");
+        
         String answer = KeyIn.nextLine();
+        
         if(answer.toLowerCase().equals("n")) 
-            break;
-
+                break;
+        
+        for(int i = 0; i < index; i++) {
+        System.out.printf("%s, %s, %s\n", names[i], emails[i], password[i]);
         }
+        
+        
+       
+        }
+        KeyIn.close();
         }
 }
