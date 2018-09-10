423f7a1a9075b5699ce4a98ad1c4d04a1ce6b4f7
Song <tjgus2932@gmail.com>
2018-09-10 16:40:28 +0900

    강사 , 매니저 DAO추가

--- java110-project/src/main/java/bitcamp/java110/cms/control/teacher/TeahcerDetailController.java
+++ java110-project/src/main/java/bitcamp/java110/cms/control/teacher/TeahcerDetailController.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import bitcamp.java110.cms.App;
 import bitcamp.java110.cms.annotation.Component;
 import bitcamp.java110.cms.annotation.RequestMapping;
+import bitcamp.java110.cms.domain.Student;
 import bitcamp.java110.cms.domain.Teacher;
 
 @Component
@@ -12,21 +13,19 @@ public class TeahcerDetailController {
     
     @RequestMapping("teacher/detail")
     public void detail(Scanner keyIn) {
-        System.out.print("조회할 번호? ");
-        int no = Integer.parseInt(keyIn.nextLine());
+        System.out.print("조회할 학생의 이메일? ");
+        String email = keyIn.nextLine();
         
-        if (no < 0 || no >= App.teachers.size()) {
-            System.out.println("무효한 번호입니다.");
+        Teacher teacher = App.teacherDao.findByEmail(email);
+        if(teacher == null) {
+            System.out.println("해당 이메일의 학생정보가 없습니다.");
             return;
         }
-        
-        Teacher t = App.teachers.get(no);
-        
-        System.out.printf("이름: %s\n", t.getName());
-        System.out.printf("이메일: %s\n", t.getEmail());
-        System.out.printf("암호: %s\n", t.getPassword());
-        System.out.printf("전화: %s\n", t.getTel());
-        System.out.printf("시급: %d\n", t.getPay());
-        System.out.printf("강의과목: %s\n", t.getSubjects());
+        System.out.printf("이름: %s\n", teacher.getName());
+        System.out.printf("이메일: %s\n", teacher.getEmail());
+        System.out.printf("암호: %s\n", teacher.getPassword());
+        System.out.printf("전화: %s\n", teacher.getTel());
+        System.out.printf("시급: %d\n", teacher.getPay());
+        System.out.printf("강의과목: %s\n", teacher.getSubjects());
     }
 }
---
+++ java110-project/src/main/java/bitcamp/java110/cms/dao/ManagerDao.java
@@ -0,0 +1,42 @@
+package bitcamp.java110.cms.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bitcamp.java110.cms.domain.Manager;
+
+    public class ManagerDao {
+    private List<Manager> list = new ArrayList<>();
+    
+    public int insert(Manager manager) {
+        for(Manager item : list) {
+            if(item.getEmail().equalsIgnoreCase(manager.getEmail())) {
+                return 0;
+            }
+        }
+        list.add(manager);
+        return 1;
+    }
+    public List<Manager> findAll() {
+        return list;
+        
+    }
+    public Manager findByEmail(String email) {
+        for(Manager item : list) {
+            if(item.getEmail().equalsIgnoreCase(email)) {
+                return item;
+            }
+        }
+        return null;
+        
+    }
+    public int delete(String email) {
+        for(Manager item : list) {
+            if(item.getEmail().equalsIgnoreCase(email)) {
+                list.remove(item);
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
