ce793c78164556336dde3e1069ffa30383d7bdae
Song <tjgus2932@gmail.com>
2018-09-14 14:12:47 +0900

    스프링IoC적용

--- java110-project/src/main/java/bitcamp/java110/cms/App.java
+++ java110-project/src/main/java/bitcamp/java110/cms/App.java
@@ -1,9 +1,12 @@
 package bitcamp.java110.cms;
 import java.util.Scanner;
 
-import bitcamp.java110.cms.context.ApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import bitcamp.java110.cms.context.RequestMappingHandlerMapping;
 import bitcamp.java110.cms.context.RequestMappingHandlerMapping.RequestMappingHandler;
+
 
 public class App {
     
@@ -11,8 +14,18 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         
+        // Spring IoC 컨테이너 사용
         ApplicationContext iocContainer = 
-                new ApplicationContext("bitcamp.java110.cms");
+                new ClassPathXmlApplicationContext
+                ("bitcamp/java110/cms/conf/application-context.xml");
+        
+        // IoC 컨테이너가 생성한 객체 조회하기
+        System.out.println("-----------------------------");
+        String[] nameList = iocContainer.getBeanDefinitionNames();
+        for(String name : nameList) {
+            System.out.println(name);
+        }
+        System.out.println("-----------------------------");
         
         RequestMappingHandlerMapping requestHandlerMap = 
                 new RequestMappingHandlerMapping();
