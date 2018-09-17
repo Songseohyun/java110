6418f1bf04648eea74fe23f5a979bee304ea652b
Song <tjgus2932@gmail.com>
2018-09-17 13:07:02 +0900

    멀티스레드 적용

--- java110-project/src/main/java/bitcamp/java110/cms/App.java
+++ java110-project/src/main/java/bitcamp/java110/cms/App.java
@@ -1,71 +1,29 @@
 package bitcamp.java110.cms;
-import java.util.Scanner;
-
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import bitcamp.java110.cms.context.RequestMappingHandlerMapping;
-import bitcamp.java110.cms.context.RequestMappingHandlerMapping.RequestMappingHandler;
 
 public class App {
     
-    static Scanner keyIn = new Scanner(System.in);
-
     public static void main(String[] args) throws Exception {
+        Thread main = Thread.currentThread();
+        System.out.println(main.getName());
         
-        // Spring IoC 컨테이너 사용
-        ClassPathXmlApplicationContext iocContainer = 
-                new ClassPathXmlApplicationContext(
-                        "bitcamp/java110/cms/conf/application-context.xml");
+        ThreadGroup mainGroup = main.getThreadGroup();
+        System.out.println(mainGroup.getName());
         
-        // IoC 컨테이너가 생성한 객체 조회하기
-        System.out.println("------------------------");
-        String[] nameList = iocContainer.getBeanDefinitionNames();
-        for (String name : nameList) {
-            System.out.println(name);
+        ThreadGroup systemGroup = mainGroup.getParent();
+        System.out.println(systemGroup.getName());
+        
+        Thread[] threads = new Thread[20];
+        int count = systemGroup.enumerate(threads, false);
+        for(int i = 0; i<count; i++) {
+            System.out.println(threads[i].getName());
         }
-        System.out.println("------------------------");
+        Thread[] tgs = new Thread[20];
+        count = mainGroup.enumerate(tgs, false);
         
-        RequestMappingHandlerMapping requestHandlerMap = 
-                new RequestMappingHandlerMapping();
-        
-        // => IoC 컨테이너에 보관된 객체의 이름 목록을 가져온다.
-        String[] names = iocContainer.getBeanDefinitionNames();
-        for (String name : names) {
-            // => 이름으로 객체를 꺼낸다.
-            Object obj = iocContainer.getBean(name);
-            
-            // => 객체에서 @RequestMapping이 붙은 메서드를 찾아 저장한다.
-            requestHandlerMap.addMapping(obj);
+        for(int i = 0; i<count; i++) {
+            System.out.println(tgs[i].getName());
         }
         
-        while (true) {
-            String menu = prompt();
-            if (menu.equals("exit")){
-                System.out.println("안녕히 가세요!");
-                break;
-            } 
-            
-            RequestMappingHandler mapping = requestHandlerMap.getMapping(menu);
-            if (mapping == null) {
-                System.out.println("해당 메뉴가 없습니다.");
-                continue;
-            }
-            
-            try {
-                mapping.getMethod().invoke(mapping.getInstance(), keyIn);
-            } catch (Exception e) {
-                System.out.println("실행 오류!");
-                System.out.println(e.getCause());
-            }
-        }
-        
-        keyIn.close();
-        iocContainer.close();
-    }
-
-    private static String prompt() {
-        System.out.print("메뉴> ");
-        return keyIn.nextLine();
     }
 }
 
